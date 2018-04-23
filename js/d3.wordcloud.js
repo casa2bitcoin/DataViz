cee71b0512578ca8371e94006fe0429d66a1beab
Leabre <lea.bresson@ensae.fr>
2018-04-23 11:27:28 +0200

    Fix word cloud

--- js/d3.wordcloud.js
+++ js/d3.wordcloud.js
@@ -2,7 +2,7 @@ var myConfig = {
   type: "wordcloud",
   options: {
     text:
-      "Elon Elon Oksman Oksman King King Szabo Szabo Szabo Satoshi Satoshi Charles Charles Marti Marti Bill Bill Graig Clear Clear Nick Nick Nick Neal Neal Shinichi Shinichi  Steven Mallmi Mallmi Michael Michael Hal Steven NSA NSA Bry Bry Gates Gates Filmey Nakamoto Nakamoto Nakamoto Wright Musk Musk Vladimir Vladimir Mochizuki Mochizuki Dorian Dorian Elon Elon Oksman Oksman King King Szabo Szabo Szabo Satoshi Satoshi Charles Charles Marti Marti Bill Bill Graig Clear Clear Nick Nick Nick Neal Neal Shinichi Shinichi  Steven Mallmi Mallmi Michael Michael Hal Steven NSA NSA Bry Bry Gates Gates Filmey Nakamoto Nakamoto Nakamoto Wright Musk Musk Vladimir Vladimir Mochizuki Mochizuki Dorian Dorian Elon Elon Oksman Oksman King King Szabo Szabo Szabo Satoshi Satoshi Charles Charles Marti Marti Bill Bill Graig Clear Clear Nick Nick Nick Neal Neal Shinichi Shinichi  Steven Mallmi Mallmi Michael Michael Hal Steven NSA NSA Bry Bry Gates Gates Filmey Nakamoto Nakamoto Nakamoto Wright Musk Musk Vladimir Vladimir Mochizuki Mochizuki Dorian Dorian Elon Elon Oksman Oksman King King Szabo Szabo Szabo Satoshi Satoshi Charles Charles Marti Marti Bill Bill Graig Clear Clear Nick Nick Nick Neal Neal Shinichi Shinichi  Steven Mallmi Mallmi Michael Michael Hal Steven NSA NSA Bry Bry Gates Gates Filmey Nakamoto Nakamoto Nakamoto Wright Musk Musk Vladimir Vladimir Mochizuki Mochizuki Dorian Dorian",
+      "Musk Szabo Finney Nakamoto Musk Wright Malmi Clear King Oksman Bry Szabo McCaleb Mochizuki Ublricht Nakamoto Wright Lehdonvirta Andresen Trammel Szabo Nakamoto Finney Wright Finney Nakamoto Szabo Szabo Wright Finney Nakamoto Szabo Musk Szabo Finney Nakamoto Musk Wright Malmi Clear King Oksman Bry Szabo McCaleb Mochizuki Ublricht Nakamoto Wright Lehdonvirta Andresen Trammel Szabo Nakamoto Finney Wright Finney Nakamoto Szabo Szabo Wright Finney Nakamoto Szabo Musk Szabo Finney Nakamoto Musk Wright Malmi Clear King Oksman Bry Szabo McCaleb Mochizuki Ublricht Nakamoto Wright Lehdonvirta Andresen Trammel Szabo Nakamoto Finney Wright Finney Nakamoto Szabo Szabo Wright Finney Nakamoto Szabo Wright Finney Nakamoto Szabo Wright Finney Nakamoto Szabo Wright Finney Nakamoto Szabo Wright Finney Nakamoto Szabo Wright Finney Nakamoto Szabo Wright Finney Nakamoto Szabo Musk Szabo Finney Nakamoto Musk Wright Malmi Clear King Oksman Bry Szabo McCaleb Mochizuki Ublricht Nakamoto Wright Lehdonvirta Andresen Trammel Szabo Nakamoto Finney Wright Finney Nakamoto Szabo Szabo Wright Finney Nakamoto Szabo Nakamoto Szabo Nakamoto Szabo Nakamoto Szabo Nakamoto Szabo Nakamoto Szabo",
     minLength: 1,
     ignore: [
       "America",
@@ -70,6 +70,83 @@ var myConfig = {
 zingchart.render({
   id: "myChart",
   data: myConfig,
-  height: 300,
-  width: 800
+  height: 350,
+  width: 500
+});
+
+
+var myConfig = {
+  type: "wordcloud",
+  options: {
+    text:
+	"PDG Américain Informaticien Américain Cryptographe Américain Japonais Américain PDG Américain Informaticien Australien Informaticien Finlandais Informaticien Irlandais Chercheur Chercheur Chercheur Informaticien Américain PDG Américain Mathématicien Japonais Informaticien Américain Américain Japonais Informaticien Australien Chercheur Finlandais Chercheur Américain PDG Américain Informaticien Américain Américain Japonais Cryptographe Américain Informaticien Australien Américain Japonais Informaticien Américain Informaticien Américain Cryptographe Américain Informaticien Australien Américain Japonais Informaticien Américain PDG Américain Informaticien Américain Cryptographe Américain Japonais Américain PDG Américain Informaticien Australien Informaticien Finlandais Informaticien Irlandais Chercheur Chercheur Chercheur Informaticien Américain PDG Américain Mathématicien Japonais Informaticien Américain Américain Japonais Informaticien Australien Chercheur Finlandais Chercheur Américain PDG Américain Informaticien Américain Américain Japonais Cryptographe Américain Informaticien Australien Américain Japonais Informaticien Américain Informaticien Américain Cryptographe Américain Informaticien Australien Américain Japonais Informaticien Américain PDG Américain Informaticien Américain Cryptographe Américain Japonais Américain PDG Américain Informaticien Australien Informaticien Finlandais Informaticien Irlandais Chercheur Chercheur Chercheur Informaticien Américain PDG Américain Mathématicien Japonais Informaticien Américain Américain Japonais Informaticien Australien Chercheur Finlandais Chercheur Américain PDG Américain Informaticien Américain Américain Japonais Cryptographe Américain Informaticien Australien Américain Japonais Informaticien Américain Informaticien Américain Cryptographe Américain Informaticien Australien Américain Japonais Informaticien Américain PDG Américain Informaticien Américain Cryptographe Américain Japonais Américain PDG Américain Informaticien Australien Informaticien Finlandais Informaticien Irlandais Chercheur Chercheur Chercheur Informaticien Américain PDG Américain Mathématicien Japonais Informaticien Américain Américain Japonais Informaticien Australien Chercheur Finlandais Chercheur Américain PDG Américain Informaticien Américain Américain Japonais Cryptographe Américain Informaticien Australien Américain Japonais Informaticien Américain Informaticien Américain Cryptographe Américain Informaticien Australien Américain Japonais Informaticien Américain PDG Américain Informaticien Américain Cryptographe Américain Japonais Américain PDG Américain Informaticien Australien Informaticien Finlandais Informaticien Irlandais Chercheur Chercheur Chercheur Informaticien Américain PDG Américain Mathématicien Japonais Informaticien Américain Américain Japonais Informaticien Australien Chercheur Finlandais Chercheur Américain PDG Américain Informaticien Américain Américain Japonais Cryptographe Américain Informaticien Australien Américain Japonais Informaticien Américain Informaticien Américain Cryptographe Américain Informaticien Australien Américain Japonais Informaticien Américain ",
+    minLength: 1,
+    ignore: [
+      "America",
+      "American",
+      "Applause",
+      "Because",
+      "because",
+      "could",
+      "don’t",
+      "people",
+      "That’s",
+      "that’s",
+      "Their",
+      "their",
+      "there",
+      "these",
+      "thing",
+      "those",
+      "through",
+      "We’re",
+      "we’re",
+      "where",
+      "would"
+    ],
+    maxItems: 40,
+    aspect: "spiral",
+    rotate: false,
+    colorType: "palette",
+    palette: [
+      "#D32F2F",
+      "#5D4037",
+      "#a83d2f",
+      "#E53935",
+      "#6D4C41",
+      "#3a2927",
+      "#F44336",
+      "#795548",
+      "#281412",
+      "#EF5350",
+      "#8D6E63",
+      "#140502"
+    ],
+
+    style: {
+      fontFamily: "Crete Round",
+
+      hoverState: {
+        borderRadius: 2
+      },
+      tooltip: {
+        text: "%text: %hits",
+        visible: true,
+
+        alpha: 0.9,
+        borderRadius: 2,
+        borderColor: "none",
+        fontFamily: "Georgia",
+        textAlpha: 1
+      }
+    }
+  },
+
+};
+
+zingchart.render({
+  id: "myChart1",
+  data: myConfig,
+  height: 350,
+  width: 500
 });
